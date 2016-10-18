152e600a2e8548025ddaca13d0c90655e3250204
wujia <wujia@DESKTOP-0R96SGO>
2016-10-18 22:19:26 +0800

    17.9

--- src/com/JES/action/AgentStudentsAction.java
+++ src/com/JES/action/AgentStudentsAction.java
@@ -1,7 +1,12 @@
 package com.JES.action;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts2.ServletActionContext;
 
 import com.JES.model.Student;
 import com.JES.service.AgentService;
@@ -31,12 +36,54 @@ public class AgentStudentsAction extends SuperAction{
 	}
 	
 	public String Mystudents(){
-		List<Student> listofstudents = new ArrayList<Student>();
-		listofstudents=agentservice.searchStudents(searchtype, searchvalue);
-		if(listofstudents!=null){
-			request.setAttribute("mystudent", listofstudents);
+		List<Student> studentList = new ArrayList<Student>();
+		studentList=agentservice.searchStudents(searchtype, searchvalue);
+		/*if(studentList!=null){
+			request.setAttribute("mystudent", studentList);
 			return "show";
 		}
-		return "noshow";
+		return "noshow";*/
+		
+		/*10.17*/
+		int totalRows = studentList.size();
+		StringBuffer sb = new StringBuffer();
+		sb.append("{\"totalCount\":\""+totalRows+"\",");  
+        sb.append("\"jsonRoot\":[");
+        for (int i=0;i<studentList.size();i++) {  
+            Student student = (Student)studentList.get(i);  
+            sb.append("{\"uid\":\""+ student.getUid());  
+            sb.append("\",");    
+            sb.append("\"stuid\":\""+ student.getStuid());  
+            sb.append("\",");    
+            sb.append("\"name\":\""+ student.getName());  
+            sb.append("\",");    
+            sb.append("\"phone\":\""+ student.getPhone());  
+            sb.append("\",");  
+            sb.append("\"weixin\":\""+ student.getWeixin());  
+            sb.append("\",");  
+            sb.append("\"sfrom\":\""+ student.getSfrom());  
+            sb.append("\"");  
+            sb.append("\"sign\":\""+ student.getSign());  
+            sb.append("\"");
+            sb.append("\"intime\":\""+ student.getIntime());  
+            sb.append("\"");
+            sb.append("\"class_\":\""+ student.getClass());  
+            sb.append("\"");
+            sb.append("\"mark\":\""+ student.getMark());  
+            sb.append("\"");
+            sb.append("},");  
+        }
+        sb.deleteCharAt(sb.lastIndexOf(","));  // 删去最后一个逗号  
+        sb.append("]}");
+        HttpServletResponse response = ServletActionContext.getResponse();           
+        try {
+        	response.setContentType("text/plain"); 
+			response.getWriter().print(sb);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        return null;
 	}
+	
 }
